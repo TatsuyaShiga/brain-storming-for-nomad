c67e64c6b760888a4ca9cfded0bbdd357edc21d7
EC2 Default User <ec2-user@ip-172-31-17-236.ap-southeast-1.compute.internal>
2019-04-30 05:02:13 +0000

    ログイン、ログアウト後の遷移は正常に行えるようになった。しかし、投稿すると、サーバーにはデータが上がるが、画面遷移は上手くいかない

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@
 # URLのリクエストを受けて、コントローラーに作業指示を行う場所です。
 Rails.application.routes.draw do
 
+# diveseのルーティングはこれで規定されている
   devise_for :users
+
 # ホーム画面関連
-  get 'tweets' => 'tweets#index'         # ホーム画面の表示
+  get '' => 'tweets#index'         # ホーム画面の表示
 
 # 会員登録関連
   get 'signin' => 'tweets#signin'  # 会員登録画面の表示
   post 'signin' => 'tweets#signin' # 会員登録フォームの内容送信
   
 # 新規投稿関連
-  get 'new' => 'tweets#new'        # 新規投稿画面の表示
-  post 'tweets' => 'tweets#create'       # 新規投稿フォームの内容送信   これあってる？
+  get 'tweets/new' => 'tweets#new'        # 新規投稿画面の表示
+  post '' => 'tweets#create'       # 新規投稿フォームの内容送信   これあってる？
   
 # ログイン関連
   get 'login' => 'tweets#login'    # ログイン画面の表示
